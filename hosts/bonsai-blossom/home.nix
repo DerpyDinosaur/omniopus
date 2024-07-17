f6a18e64ad928a5a0f7d58c1fd9dd480b0a67918
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2024-07-17 21:08:03 +0800

    wallpaper

--- hosts/bonsai-blossom/home.nix
+++ hosts/bonsai-blossom/home.nix
@@ -9,7 +9,7 @@
   # home.file."wallpapers" = {
   #   source = "../../wallpapers";
   # };
-  home.file."wallpapers".source = ../../wallpapers;
+  home.file.".wallpapers".source = ../../wallpapers;
 
   home.stateVersion = "23.11";
   # Let Home Manager install and manage itself.
