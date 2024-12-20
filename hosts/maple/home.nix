48edd0fca39f64c8a20433df40e3d6a2389d03ec
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2024-12-20 14:45:42 +0800

    maple home config

--- hosts/maple/home.nix
+++ hosts/maple/home.nix
@@ -1,1 +1,48 @@
-home.nix
+{ pkgs, username, ... }:
+
+{
+  # Configs
+  home.file.".config/sway/config".source = ../../programs/sway/config;
+  home.file.".config/rofi-themes".source = ../../programs/rofi;
+
+  ## ZSH
+  programs.zsh = {
+    enable = true;
+
+    shellAliases = {
+      battery = "cat /sys/class/power_supply/BAT0/capacity";
+    };
+  };
+
+  ## Foot
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        term = "xterm-256color";
+        dpi-aware = "yes";
+        font = "monospace:size=14";
+        font-size-adjustment = 2;
+      };
+      colors = {
+        alpha=0.8;
+      };
+      mouse = {
+        hide-when-typing = "yes";
+      };
+    };
+  };
+
+  # Rofi
+  programs.rofi = {
+    enable = true;
+    theme = "/home/${username}/.config/rofi-themes/squared-evergreen.rasi";
+  };
+
+  # Wallpapers
+  home.file."pictures/wallpapers".source = ../../wallpapers;
+
+  home.stateVersion = "24.11";
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
